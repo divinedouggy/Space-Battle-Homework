acf93ed4b5a37467566447cedc3f861eef7dd95d
divinedouggy <105749979+divinedouggy@users.noreply.github.com>
2022-06-18 22:02:05 -0400

    Game working, one-shot, no turns yet, 1 enemy

--- script.js
+++ script.js
@@ -31,32 +31,62 @@ class spaceShip {
 const yourShip = new spaceShip(20, 5, .7);
 
 // Alien Object
+
+
 const alienShip = new spaceShip(alienHull, alienFirepower, alienAccuracy)
 
+function playGame() {
+    newAlienShip = alienShip;
+    yourAttack();
+    
+    function yourAttack() {
+        if (Math.random() < .7) {
+            console.log('You hit the alien scum!');
+            console.log(newAlienShip.hull)
+            newAlienShip.hull -= 5;
+            console.log(newAlienShip.hull)
+        } else {
+            console.log('Dang! You missed!');
+        } if (newAlienShip.hull <= 0) {
+            return console.log("Alien dead! You win!")
+        } else {
+            console.log("The alien is attacking!");
+            enemyAttack()
+        }
 
+    }  // end of yourAttack function
 
-// My attack
-function yourAttack() {
-
-    if (Math.random() < .7) {
-        // alienShip.hull -= yourShip.firepower
-        console.log('You hit the alien scum!');
-        console.log(alienShip.hull)
-        alienShip.hull -= 5;
-        console.log(alienShip.hull)
-        console.log('')
-    } else {
-        console.log('Dang! You missed!');
-    } 
-}
-yourAttack()
-
-// Their attack
-function enemyAttack () {
-    if (Math.random() < alien[0].accuracy) {
-        target.hull -= 
+function enemyAttack() {
+    if (Math.random() < newAlienShip.accuracy) {
         console.log('You have been hit!');
-    }
+        console.log(yourShip.hull)
+        yourShip.hull -= newAlienShip.firepower;
+        console.log(yourShip.hull)
+        } else {
+            console.log('Phew! That was close!')
+        } if (yourShip.hull <= 0) {
+            return console.log("NOOOO! YOU'VE BEEN DEFEATED!")
+        } else {
+            console.log("You're still alive!");
+           yourAttack()
+        }
 }
+    
+ 
+
+
+} // end of playGame function
+
+
+playGame() // calling playGame
+
+
+
+
+
+
+
+
+
 
 
